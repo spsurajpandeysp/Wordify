5d58a5109145abb181a3959a9753c351e98a0cb6
spsurajpandeysp <surajpandey7493@gmail.com>
2025-10-25 21:52:04 +0530

    env port config

--- backend/server_clean.py
+++ backend/server_clean.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     
     uvicorn.run(
         "server_clean:app",
-        host="localhost", 
-        port=4000,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
         reload=True
     )
